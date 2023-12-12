9d6f38a7dd61a71d97af858d2a0960e4c8d6bc31
Aaron <aaronromeromusic@gmail.com>
2023-12-12 20:15:42 +0100

    assets me mata

--- public/header/nav/logos/pintaesta.js
+++ public/header/nav/logos/pintaesta.js
@@ -6,7 +6,7 @@ const pintaesta = () => {
   const pintaestaLogo = document.createElement('img');
   pintaestaLogo.className = 'pintaesta_logo';
   pintaestaLogo.alt = 'pintaesta_logo';
-  pintaestaLogo.src = '../../../assets/img/Pinterest-logo.png';
+  pintaestaLogo.src = './assets/img/Pinterest-logo.png';
 
   logoUrl.appendChild(pintaestaLogo);
 
